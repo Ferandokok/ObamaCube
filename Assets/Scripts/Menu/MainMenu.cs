24fbb3ed4538a870b42230bc933a333f37eacaa0
Christian Oosterveen <c.oosterveen@sintlucasedu.nl>
2023-03-20 10:22:46 +0100

    Credits

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+
+
+
+
+
+}
