21e472a917432445425fe66d0361f9d54886d23a
Fernando <F.kok@sintlucasedu.nl>
2023-03-23 14:32:01 +0100

    name shower

--- Assets/Scripts/NameSelection.cs
+++ Assets/Scripts/NameSelection.cs
@@ -7,29 +7,41 @@ public class NameSelection : MonoBehaviour
 {
 
     public TMP_InputField nameInput;
+
     public TMP_Text playerNameText;
+    public TMP_Text playersList;
+    public TMP_Text playerAmount;
+
     public Button submitButton;
+<<<<<<< Updated upstream
+=======
+    
+>>>>>>> Stashed changes
     public int maxPlayers = 5;
     public List<string> playerNames = new List<string>();
+
+    
 
     private int currentPlayer = 0;
 
     void Start()
     {
-        playerNameText.text = "Player " + (currentPlayer + 1);
+        //playerNameText.text = "Player " + (currentPlayer + 1);
         submitButton.onClick.AddListener(SubmitName);
     }
 
     void SubmitName()
     {
+        
         if (nameInput.text != "")
         {
             playerNames.Add(nameInput.text);
             currentPlayer++;
-
+            
             if (currentPlayer < maxPlayers)
             {
-                playerNameText.text = "Player " + (currentPlayer + 1);
+                
+                //playerNameText.text = "Player " + (currentPlayer + 1);
                 nameInput.text = "";
             }
             else
@@ -38,6 +50,13 @@ public class NameSelection : MonoBehaviour
                 nameInput.gameObject.SetActive(false);
                 submitButton.gameObject.SetActive(false);
             }
+            playersList.text = "";
+            foreach (string playerName in playerNames)
+            {
+                playersList.text += playerName + "\n";
+            }
+            playerAmount.text = "Players: " + playerNames.Count;
+
         }
     }
 }
